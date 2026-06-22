047473c22766fe688b8b14bf3b8e59a159f4bf0f
Olena Zozulia <lenazozula05@gmail.com>
2026-06-22 17:09:29 +0300

    Fortify authorization configured, login forms and admin panel added

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <title>Адмін-панель</title>
+</head>
+<body>
+    <h1>Вітаю в адмінці!</h1>
+    <p>Це ваш перший шаблон для адмін-панелі.</p>
+</body>
+</html>
